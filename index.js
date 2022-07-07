ef36a34536e86639bcfa7b531c0c5a8294f5c416
Poer99999 <108895410+Poer99999@users.noreply.github.com>
2022-07-08 02:15:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
-const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
+const BotName = 'PBot'; 
+const instagram = 'https://instagram.com/purwo_kok'; 
+const whatsapp = 'https://wa.me/6281568390700'; 
+const kapanbotaktif = 'KETIKA KAMU MERINDUKANKU'; 
 const youtube = 'https://www.youtube.com/c/reuz_id';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
@@ -68,7 +68,7 @@ conn.on('user-presence-update', json => console.log(json.id + ' presence is => '
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
-   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@reuz.id`)
+   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by Poer`)
 })
 
 conn.on('message-new', async(m) =>
